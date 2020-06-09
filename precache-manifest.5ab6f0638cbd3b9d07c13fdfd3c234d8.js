7b4ef1eed2ecc451d78e7bf78f8372ca2853d0da
Magnus <magnus.e@outlook.com>
2020-06-09 19:39:26 +0200

    Updates

---
+++ precache-manifest.5ab6f0638cbd3b9d07c13fdfd3c234d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e349c3773b4fe0823580341f034ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9393c48938aa557d3d51",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cc13262a84b051a64cc6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9393c48938aa557d3d51",
+    "url": "/static/js/2.32194215.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.32194215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc13262a84b051a64cc6",
+    "url": "/static/js/main.5322b2c0.chunk.js"
+  },
+  {
+    "revision": "8fb9da8146782bf15b92",
+    "url": "/static/js/runtime-main.703cc913.js"
+  }
+]);
